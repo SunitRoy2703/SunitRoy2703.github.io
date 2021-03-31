98b634fab5af7a1837c9466a3770a852c603bca5
SunitRoy2703 <2703iamsry@gmail.com>
2021-03-31 22:18:30 +0000

    Deploying to gh-pages from  @ 6bd74804b4eda2bb7b649d7aa092796107829fdc 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fec05f1e92b2e3a2e435b40eeb91b266.js"
+  "/precache-manifest.efea993d571b8225ac411e7324e43e84.js"
 );
 
 self.addEventListener('message', (event) => {
