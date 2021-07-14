a9345901b821c5012bd0ca729fa15ba1529ea777
Sunit Roy <2703iamsry@gmail.com>
2021-07-15 04:01:57 +0530

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,10 +12,10 @@ const greeting = {
   username: "Sunit Roy",
   title: "Hi all, I'm Sunit",
   subTitle: emoji(
-    "A passionate Software Developer 🚀 having mobile development work experience in Android, Flutter, Augmented Reality, Machine Learning .  I'm currently working in a start-up called Wholesome Company."
+    "A passionate Software Developer 🚀 having mobile development work experience in Android, Flutter, IOS & Augmented Reality.  I'm currently contributing to Google's Open Source Machine Learning Platform Tensorflow."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1J3wzvp4HBxAyA-tRWXD3spiyp0sn0-zz/view?usp=sharing",
+    "https://docs.google.com/document/d/1JTJUpKhgkWnAUTnAbvbjgJPSkt8klW8mkOYgKxdmsW8/edit?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -27,7 +27,7 @@ const socialMediaLinks = {
   gmail: "iamsunitroy03@gmail.com",
   // gitlab: "https://gitlab.com/saadpasta",
   // facebook: "https://www.facebook.com/saad.pasta7",
-   medium: "https://sunitroy.medium.com/",
+  medium: "https://sunitroy.medium.com/",
   // // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
@@ -139,7 +139,7 @@ const educationInfo = {
       duration: "January 2013 - April 2019",
       // desc:
       //   "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-     // descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      // descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
     }
   ]
 };
@@ -150,7 +150,7 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Android Framework", //Insert stack or technology you have experience in
+      Stack: "Android SDK", //Insert stack or technology you have experience in
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
@@ -159,15 +159,15 @@ const techStack = {
     },
     {
       Stack: "Flutter Framework",
-      progressPercentage: "90%"
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "IOS SDK",
+      progressPercentage: "65%"
     },
     {
       Stack: "Firebase",
       progressPercentage: "75%"
-    },
-    {
-      Stack: "Rest Api",
-      progressPercentage: "60%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -179,18 +179,32 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
+      role: "GSoc Student Developer",
+      company: "Tensorflow",
+      companylogo: require("./assets/images/TF_Icon.jpg"),
+      date: "May 2021 - Present",
+      desc:
+        "Contributing to Google's Open Source Machine Learning Platform Tensorflow in collaboration with Google Engineers",
+
+      descBullets: [
+        "Recreated the existing Bert Qa example in Android(Java) and IOS(Swift) with TFLite Library",
+        "Recreated the existing Text Classification example in Android(Java) and IOS(Swift) with TFLite Library",
+        "Wrote tests and documentation for each example Application",
+      ]
+    },
+    {
       role: "Junior Software Developer",
       company: "Wholesome Company",
       companylogo: require("./assets/images/wholesome.webp"),
-      date: "Mar 2021 - Present",
+      date: "Mar 2021 - May 2021",
       desc:
-        "Recently, I got promoted to Junior Software Developer in Wholesome Company, Now I'm helping the start up in Research and building docs",
-     
-         descBullets: [
-        "Research",
-        "Adding new features",
-        "App Security",
-        "Building docs"
+        "I got promoted to Junior Software Developer in Wholesome Company, Now I'm helping the start up in Research and building docs",
+
+      descBullets: [
+        "Designed & Developed an Augmented Reality Social media platform(Java)",
+        "Took an Android Development webinar with 50 students",
+        "Researched Augmented Reality & 3D rendering on mobile",
+        "Building docs & diagrams"
       ]
     },
     {
@@ -200,8 +214,8 @@ const workExperiences = {
       date: "Dec 2020 – Mar 2021",
       desc:
         "I was a Mobile Developer intern in Wholesome Company, I have helped the start up for developing multiple AR apps from scratch, and deploy it to the Playstore.",
-     
-         descBullets: [
+
+      descBullets: [
         "Modifying their previous projects",
         "Improving their UI/UX",
         "Adding new features",
@@ -219,29 +233,23 @@ const workExperiences = {
       date: "Aug 2020 – Dec 2020",
       desc:
         "Last year I got involved in Fossasia, an One of the biggest Open Source Organization in Asia.",
-     descBullets: [
-          "Got Involved in multiple projects",
-          "Multiple pull request to their code",
-          "Many contributions related to the community",
-          "Participated in multiple virtual events there",
-        ]  
-      },
-  //  {
-    //   role: "Software Engineer Intern",
-    //   company: "Airbnb",
-    //   companylogo: require("./assets/images/airbnbLogo.png"),
-    //   date: "Jan 2015 – Sep 2015",
-    //   desc:
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    // }
+      descBullets: [
+        "Got Involved in multiple projects",
+        "Multiple pull request to their code",
+        "Many contributions related to the community",
+        "Participated in multiple virtual events there",
+      ]
+    }
   ]
 };
+
+
 
 /* Your Open Source Section to View Your Github Pinned Projects
 To know how to get github key look at readme.md */
 
 const openSource = {
-  githubConvertedToken:  process.env.REACT_APP_GITHUB_TOKEN,
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
   githubUserName: "SunitRoy2703", // Change to your github username to view your profile in Contact Section.
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
@@ -253,6 +261,18 @@ const bigProjects = {
   title: "Projects",
   subtitle: "PROJECTS THAT I HAVE DEVELOPED AND CONTRIBUTED TO",
   projects: [
+    {
+      image: require("./assets/images/help_out.png"),
+      projectName: "helpOut",
+      projectDesc: "helpOut lets you search for Oxygen, Ambulance, Ventilator, Beds, and many more related tweets by location.",
+      footerLink: [
+        {
+          name: "Get The App in PlayStore",
+          url: "https://play.google.com/store/apps/details?id=com.sunit.helpout"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
     {
       image: require("./assets/images/ic_launcher_foreground.png"),
       projectName: "Essence(WHOLESOME COMPANY)",
@@ -326,17 +346,7 @@ const achievementSection = {
           name: "Certification",
           url:
             "https://drive.google.com/file/d/1bjHWivIl0HvxcH_zmka4kDp5r3BV6vNA/view?usp=sharing"
-        },
-        // {
-        //   name: "Award Letter",
-        //   url:
-        //     "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        // },
-        // {
-        //   name: "Google Code-in Blog",
-        //   url:
-        //     "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        // }
+        }
       ]
     },
     {
@@ -358,8 +368,10 @@ const achievementSection = {
       subtitle: "Crash Course on Python by Google",
       image: require("./assets/images/google.png"),
       footerLink: [
-        {name: "Certification", 
-        url: "https://drive.google.com/file/d/1gskIM0aH49Ifwnxujp16fM8AKbSsVJ6U/view?usp=sharing"},
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/1gskIM0aH49Ifwnxujp16fM8AKbSsVJ6U/view?usp=sharing"
+        },
       ]
     }
   ],
@@ -375,18 +387,43 @@ const blogSection = {
 
   blogs: [
     {
+      url: "https://sunitroy.medium.com/android-development-series-for-beginners-46001562eb42",
+      title: "Android Development Series for Beginners🚀",
+      description:
+        "I’m really excited to announce the Android development series. This series is specially for beginners in Android and will teach you how to build and deploy Android Applications with very efficient methods."
+    },
+    {
+      url: "https://sunitroy.medium.com/learn-to-make-jetpack-compose-in-10-minutes-a27335a4a225",
+      title: "Learn to make Jetpack Compose in 10 minutes!",
+      description:
+        "Jetpack Compose is a modern toolkit for building native Android UI. Jetpack Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin API"
+    },
+    {
+      url: "https://sunitroy.medium.com/create-a-branch-from-unstaged-uncommitted-changes-on-master-e8bbbd602797",
+      title: "Create a branch from unstaged/uncommitted changes on master",
+      description:
+        "You might have started to make changes to your local repo and later released that you are working on the master branch"
+    },
+    {
+      url: "https://sunitroy.medium.com/do-you-need-to-install-jdk-for-installing-android-studio-b8cade1f1e4d",
+      title: "Do you need to install JDK for installing Android Studio📝",
+      description:
+        "It’s very surprising that big Youtubers, even guys with Million subscriber tells you to install standalone JDK before Android studio."
+    },
+    {
+      url: "https://sunitroy.medium.com/error-pathspec-xxx-did-not-match-any-file-s-known-to-git-f3086153f054",
+      title: "error: pathspec ‘XXX’ did not match any file(s) known to git",
+      description:
+        "You might be here because you searched the above title on Google (as I did yesterday when I faced the same problem) and after getting the results, you preferred Medium over StackOverflow. :)"
+    },
+    {
       url:
         "https://sunitroy.medium.com/how-to-use-firebase-on-android-without-the-google-services-plugin-d02c8b9854b7",
       title: "Use Firebase without the google-services.json file in Android",
       description:
         "Do you want to know how to use firebase without google plug-in?"
     },
-    // {
-    //   url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-    //   title: "Why REACT is The Best?",
-    //   description:
-    //     "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    // }
+
   ],
   display: true // Set false to hide this section, defaults to true
 };
